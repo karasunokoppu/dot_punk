c7593b79f1e7579e2cd086d6e2213778fc45d35c
noruno <crowprogram@gmail.com>
2025-09-01 23:09:06 +0900

    テキストボックスの更新処理を実装

--- src/game/world/npc/components.rs
+++ src/game/world/npc/components.rs
@@ -41,3 +41,30 @@ impl Default for NPC {
         }
     }
 }
+impl NPC {
+    pub fn new(id: u32) -> Self {
+        NPC {
+            id: id,
+            name: format!("Citizen {}", id),
+            npc_type: NPCType::Generic(TalkDialog::new(id)),
+            sprite: SpriteData {
+                z_index: 10, //10..19
+                image: "default_npc_image.png".to_string(),
+            },
+            position: Position::default(),
+            direction: Direction::default(),
+            states: EntityStates::default(),
+        }
+    }
+    pub fn with_position(self: Self, position: Position) -> Self {
+        NPC {
+            id: self.id,
+            name: self.name,
+            npc_type: self.npc_type,
+            sprite: self.sprite,
+            position: position,
+            direction: self.direction,
+            states: self.states,
+        }
+    }
+}
